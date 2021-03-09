ffca07399aff72fe398c716b23f8e16dfeec5b70
hwookim <gusdn9754@gmail.com>
2021-03-09 10:44:23 +0900

    feat: 변수 활용하기

--- Assets/HelloCode.cs
+++ Assets/HelloCode.cs
@@ -7,7 +7,17 @@ public class HelloCode : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("hello world!");
+        string name = "리라";
+        char bloodType = 'A';
+        int age = 17;
+        float height = 168.3f;
+        bool isFemale = true;
+
+        Debug.Log("캐릭터 이름 : " + name);
+        Debug.Log("혈액형 : " + bloodType);
+        Debug.Log("나이 : " + age);
+        Debug.Log("키 : " + height);
+        Debug.Log("여성인가? : " + isFemale);
     }
 
     // Update is called once per frame

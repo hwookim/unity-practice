951a9460f5bd79dd702a8c42fb3ac55454405b6e
hwookim <gusdn9754@gmail.com>
2021-03-16 10:16:58 +0900

    feat: 챕터5 과제
     - Rigidbody 객체를 통해 GameObjcet 점프시키기

--- Assets/Assignment5.cs
+++ Assets/Assignment5.cs
@@ -1,13 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Text.RegularExpressions;
 
 public class Assignment5 : MonoBehaviour
 {
+    public Rigidbody rigidbody;
+    public GameObject gameObject;
+
+    int[] jumperForce = {500, 1000};
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        string name = gameObject.name;
+        int number = int.Parse(Regex.Replace(name, "[^0-9]", "")) - 1;
+        Debug.Log(name + " jump " + jumperForce[number]);
+
+        rigidbody.AddForce(0, jumperForce[number], 0);
     }
 
     // Update is called once per frame

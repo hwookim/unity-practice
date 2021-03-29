0879ee0dbee385c3b4e1b1126ca03de7242e6b06
hwookim <gusdn9754@gmail.com>
2021-03-29 20:54:24 +0900

    feat: 탄알 생성기 구현

---
+++ Assets/Dodge/BulletSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawner : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public float spawnRateMin = 0.5f;
+    public float spawnRateMax = 3f;
+
+    private Transform target;
+    private float spawnRate;
+    private float timeAfterSpwan;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeAfterSpwan = 0f;
+        spawnRate = Random.Range(spawnRateMin, spawnRateMax);
+        target = FindObjectOfType<PlayerController>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeAfterSpwan += Time.deltaTime;
+
+        if (timeAfterSpwan >- spawnRate) {
+            timeAfterSpwan = 0f;
+
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+            bullet.transform.LookAt(target);
+
+            spawnRate = Random.Range(spawnRateMin, spawnRateMax);
+        }
+        
+    }
+}

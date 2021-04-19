68dbcaa13645c7f3c27bcf41aac05528f6aae8b5
hwookim <gusdn9754@gmail.com>
2021-04-19 19:51:37 +0900

    feat: 사망 시 텍스트 표시 및 재시작 구현

--- Uni-Run/Assets/Scripts/GameManager.cs
+++ Uni-Run/Assets/Scripts/GameManager.cs
@@ -34,6 +34,9 @@ public class GameManager : MonoBehaviour {
 
     void Update() {
         // 게임 오버 상태에서 게임을 재시작할 수 있게 하는 처리
+        if (isGameover && Input.GetMouseButtonDown(0)) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
 
     // 점수를 증가시키는 메서드
@@ -43,6 +46,7 @@ public class GameManager : MonoBehaviour {
 
     // 플레이어 캐릭터가 사망시 게임 오버를 실행하는 메서드
     public void OnPlayerDead() {
-        
+        isGameover = true;
+        gameoverUI.SetActive(true);
     }
 }

4901a51343967789941a42c7df62aef353c03035
hwookim <gusdn9754@gmail.com>
2021-05-10 19:30:03 +0900

    중간고사

---
+++ Middleterm/Assets/Scenes/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject text;
+    public Text timeText;
+    public Text scoreText;
+
+    private float playTime;
+    private bool isEnd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playTime = 15;
+        isEnd = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isEnd) {
+            playTime -= Time.deltaTime;
+            timeText.text = "Time: " + (int) playTime;
+        }
+    }
+
+    public void EndGame() {
+        isEnd = true;
+        ShowGameEndText();
+    }
+
+    void ShowGameEndText() {
+        float score = PlayerPrefs.GetFloat("Score");
+        scoreText.text = "Score: " + (int) score;
+    }
+}

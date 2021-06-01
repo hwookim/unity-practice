5478e731d2fb6f3808d6548d3e4d347fa862a497
hwookim <gusdn9754@gmail.com>
2021-06-01 17:45:56 +0900

    feat: 가상 카메라 설정

--- Zombie Multiplayer/Assets/Scripts/CameraSetup.cs
+++ Zombie Multiplayer/Assets/Scripts/CameraSetup.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 // 시네머신 카메라가 로컬 플레이어를 추적하도록 설정
 public class CameraSetup : MonoBehaviourPun {
     void Start() {
-        
+        // 만약 자신이 로컬 플레이어라면 가상 카메라 추적 대상 변경
+        if (photonView.IsMine)
+        {
+            CinemachineVirtualCamera followCam = FindObjectOfType<CinemachineVirtualCamera>();
+
+            followCam.Follow = transform;
+            followCam.LookAt = transform;
+        }
     }
 }
